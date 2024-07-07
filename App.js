829751de503d275dd5c98b36fcdc3351f0e2be0f
mariemaloui22 <149721742+mariemaloui22@users.noreply.github.com>
2024-07-07 21:21:24 +0200

    Ecommerce website
    using Api fake to do e-commerce website

---
+++ App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Men from './Men';
+import Women from './Women';
+import Electronics from'./Electronics';
+
+function App() {
+  return (
+   <div>
+     <div className="flex gap-2 bg-slate-200 font-semibold">
+        <a className="p-2" href="/men" >Men</a>
+        <a className="p-2" href="/Women" >Women</a>
+        <a className="p-2" href="/electronics" >Electronics</a>
+</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/men" element={<Men />} />
+          <Route path="/women" element={<Women />} />
+          <Route path="/electronicshttps://github.com/mariemaloui22/Ecommerce-project.git" element={<Electronics/>} />
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ Electronics.js
@@ -0,0 +1,43 @@
+
+import React, { useEffect, useState } from 'react';
+
+const  Electronics=()=> {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        if (!response.ok) {
+          throw new Error('Could not fetch data');
+        }
+        const data = await response.json();
+        const electronicsProducts = data.filter(product => product.category === "electronics");
+        setProducts(electronicsProducts);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h2 className="text-3xl font-semibold mb-4">Electronics Products</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {products.map(product => (
+          <div key={product.id} className="bg-white shadow-md p-4">
+            <h3 className="text-lg font-semibold mb-2">{product.title}</h3>
+            <p className="text-gray-60">{product.description}</p>
+            <img src={product.image} alt=''className="w-80 h-80 object-contain"/>  
+            <p className="text-gray-800 font-semibold mt-2">Price: ${product.price}</p>
+          </div>
+        ))}
+      </div>
+   
+    </div>
+  );
+};
+export default Electronics;
+
